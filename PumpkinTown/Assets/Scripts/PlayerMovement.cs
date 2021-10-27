e15fa4f7c17937abf8cdeb541fb526faa4015aa7
OlleKilgarrah <olle.reberg@student.forsbergsskola.se>
2021-10-27 16:22:43 +0200

    TRying to fix checkifgrounded

--- PumpkinTown/Assets/Scripts/PlayerMovement.cs
+++ PumpkinTown/Assets/Scripts/PlayerMovement.cs
@@ -33,25 +33,38 @@ public class PlayerMovement : MonoBehaviour
     private void Move()
     {
         isGrounded = Physics.CheckSphere(transform.position, groundCheckDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
         
         float moveZ = Input.GetAxis("Vertical");
         moveDirection = new Vector3(0, 0, moveZ);
-        if (moveDirection != Vector3.zero && !Input.GetKey(KeyCode.LeftShift))
-        {
-            Walk();
-        }
-        else if (moveDirection != Vector3.zero && Input.GetKey(KeyCode.LeftShift))
-        {
-            Run();
-        }
-        else if (moveDirection == Vector3.zero)
-        {
-            Idle();
-        }
 
+        if (isGrounded)
+        {
+            if (moveDirection != Vector3.zero && !Input.GetKey(KeyCode.LeftShift))
+            {
+                Walk();
+            }
+            else if (moveDirection != Vector3.zero && Input.GetKey(KeyCode.LeftShift))
+            {
+                Run();
+            }
+            else if (moveDirection == Vector3.zero)
+            {
+                Idle();
+            }
+
+        }
+        
         moveDirection *= moveSpeed;
 
         controller.Move(moveDirection * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
     }
 
     private void Idle()

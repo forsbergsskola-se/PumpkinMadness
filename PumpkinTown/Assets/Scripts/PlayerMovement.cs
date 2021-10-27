0a06f479206a63c314e99f572b22a5b5683c05c7
OlleKilgarrah <olle.reberg@student.forsbergsskola.se>
2021-10-27 14:46:08 +0200

    Playermovment script

---
+++ PumpkinTown/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //Variables
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float walkSpeed;
+    [SerializeField] private float runSpeed;
+
+    private Vector3 moveDirection;
+    
+    //References
+    private CharacterController controller;
+}

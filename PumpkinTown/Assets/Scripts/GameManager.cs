49c1f77d348dac91628d1f4453fe73e90164a34b
OlleKilgarrah <olle.reberg@student.forsbergsskola.se>
2021-10-29 12:23:08 +0200

    Added structure to gamemanager

--- PumpkinTown/Assets/Scripts/GameManager.cs
+++ PumpkinTown/Assets/Scripts/GameManager.cs
@@ -6,10 +6,50 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+   public static GameManager Instance;
+   public float restartDelay = 2f;
+   public GameState State;
+   public static event Action<GameState> OneGameStateChanged; 
+
    private bool gameHasEnded = false;
 
-   public float restartDelay = 2f;
-   
+   private void Awake()
+   {
+      Instance = this;
+   }
+
+   private void Start()
+   {
+      UpdateGameState(GameState.StartGame);
+   }
+
+   public void UpdateGameState(GameState newState)
+   {
+      State = newState;
+
+      switch (newState)
+      {
+         case GameState.StartGame:
+            HandleGameStart();
+            break;
+         case GameState.Play:
+            break;
+         case GameState.Victory:
+            break;
+         case GameState.Lose:
+            break;
+         default:
+            throw new ArgumentOutOfRangeException(nameof(newState), newState, null);
+      }
+
+      OneGameStateChanged?.Invoke(newState);
+   }
+
+   private void HandleGameStart()
+   {
+     
+   }
+
    public void GameOver()
    {
       if (gameHasEnded == false)
@@ -25,4 +65,13 @@ public class GameManager : MonoBehaviour
    {
       SceneManager.LoadScene(SceneManager.GetActiveScene().name);
    }
+   
+  
+}
+public enum GameState 
+{
+   StartGame,
+   Play,
+   Victory,
+   Lose
 }

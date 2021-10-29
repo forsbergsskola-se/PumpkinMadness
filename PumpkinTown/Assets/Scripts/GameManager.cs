ff56fbc15b8ad7e9720831dead00c9d32b7346c5
OlleKilgarrah <olle.reberg@student.forsbergsskola.se>
2021-10-29 11:10:42 +0200

    Created gamemanager

---
+++ PumpkinTown/Assets/Scripts/GameManager.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+   void GameOver()
+   {
+      Debug.Log("GAME OVER");
+   }
+}

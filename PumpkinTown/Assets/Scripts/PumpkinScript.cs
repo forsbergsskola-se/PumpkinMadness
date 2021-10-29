5797b5106c83870fdfe53e6e7df5904af8a0912b
invinsiblee <elias.bernmarker@hotmail.com>
2021-10-29 14:27:17 +0200

    Fixat pumpa script

--- PumpkinTown/Assets/Scripts/PumpkinScript.cs
+++ PumpkinTown/Assets/Scripts/PumpkinScript.cs
@@ -4,16 +4,52 @@ using UnityEngine;
 
 public class PumpkinScript : MonoBehaviour
 {
+    bool inside = false;
+    bool startTime = false;
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
-            Destroy(gameObject);
-            ScoreScript.scoreValue += 1;
-            Debug.Log("Object destroyd");
+            startTime = true;
+            
+        //StartCoroutine(pumpkinTimer());
+        //IEnumerator pumpkinTimer()
+        //{
+        //startTime = true;
+        //yield return new WaitForSeconds(3);
+        //Destroy(gameObject);
+        //ScoreScript.scoreValue += 1;
+        //inside = true;
+        //Debug.Log("Object destroyd");
+        //}
+        //if (inside == false)
+        //{
+        //StopCoroutine(pumpkinTimer());
+        //}
+        
+        }        
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        startTime = false;        
+    }
+    private void FixedUpdate()
+    {
+        if (startTime == true)
+        {
+            StartCoroutine(pumpkinTimer());
+            IEnumerator pumpkinTimer()
+            {
+                Debug.Log("StartTimetrue");
+                yield return new WaitForSeconds(3);
+                Destroy(gameObject);
+            }
         }
-
-
+        if (startTime == false)
+        {
+            StopAllCoroutines();
+            Debug.Log("Exit");
+        }
     }
 }
 

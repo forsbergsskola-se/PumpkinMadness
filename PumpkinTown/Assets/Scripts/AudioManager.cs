7a5a77b405bedb21f143880bccd3695639c477f5
OlleKilgarrah <olle.reberg@student.forsbergsskola.se>
2021-10-29 17:15:47 +0200

    Jump fixat

---
+++ PumpkinTown/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    
+   
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}

f2420a9d9e3600ae2c9db4f153f08cd90b1353a1
invinsiblee <elias.bernmarker@hotmail.com>
2021-10-29 13:37:19 +0200

    Enemy follow and kill player script

---
+++ PumpkinTown/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    public Transform target;
+    public float speed = 4f;
+    Rigidbody rig;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody>();        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //Make enemy move towards player
+        Vector3 pos = Vector3.MoveTowards(transform.position, target.position, speed * Time.fixedDeltaTime);
+        rig.MovePosition(pos);
+        transform.LookAt(target);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+            Destroy(GameObject.Find("Elias-Player"));
+    }
+}

129c9d080097d99f4a3f18cdfc0a28fc3d1017a2
OlleKilgarrah <olle.reberg@student.forsbergsskola.se>
2021-10-29 19:21:02 +0200

    Timer starts when entering Field

--- PumpkinTown/Assets/Scripts/CameraMove.cs
+++ PumpkinTown/Assets/Scripts/CameraMove.cs
@@ -8,12 +8,14 @@ public class CameraMove : MonoBehaviour
     {
         if(other.tag == "Player")
         {
+            GameManager _gameManager = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameManager>();
+            _gameManager.UpdateGameState(GameState.StartGame);
             GameObject.Find("MainCamera").transform.position = transform.position + new Vector3(3, 15, -5);
             GameObject.Find("FenceClose").transform.position = transform.position + new Vector3(1, -1, 0);
             GameObject.Find("EnemyCollider2").transform.position = transform.position + new Vector3(-50, 0, 0);
             GameObject.Find("EnemyCollider3").transform.position = transform.position + new Vector3(-50, 0, 0);
             GameObject.Find("EnemyCollider4").transform.position = transform.position + new Vector3(-50, 0, 0);
             GameObject.Find("EnemyCollider5").transform.position = transform.position + new Vector3(-50, 0, 0);
-        }        
+        }
     }
 }
